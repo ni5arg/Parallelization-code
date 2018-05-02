b4171793ae65f5725001948331375c64a68b50f4
ni5arg <35376387+ni5arg@users.noreply.github.com>
2018-05-02 09:16:53 -0300

    Reduced time
    No repeated children

--- main.py
+++ main.py
@@ -6,46 +6,59 @@ Created on Tue Apr 24 18:48:36 2018
 """
 import requests
 import time
-#import multiprocessing
+import multiprocessing as mp
 #import threading
 
-def fetch(url):
+
+
+
+def fetch(urls, reward_lst):
+    for url in urls:
+        json_data = requests.get(url).json()
+        try:
+
+            children = list(set(json_data['children'])) #No duplicates
+            for i in children:
+                urls.append(i)
+        except:
+            print('Tree end')
+
+        reward = json_data['reward']
+        reward_lst.append(reward)
+
+
+def run():
+    core_num = mp.cpu_count()
+    bucket_size = (len(urls)//core_num) + 1
+    reward_lst = mp.Manager().list()
+    jobs = []
+    for i in range(core_num):
+        url_bucket = urls[i*bucket_size,(i+1)*bucket_size]
+        p = mp.process(target=fetch, args=(url_bucket,reward_lst,))
+        p.start()
+        jobs.append(p)
+    [p.join() for p in jobs]
     
-    ''' Takes the url as the input. Returns JSON and extracts 'children' and 'reward' values. '''
-
-    #print()
-    json_data = requests.get(url).json()
-    try:
-        
-        children = list(json_data['children']) #No duplicate children
- 
-        for i in children:
-            next_url.append(i)
-    except:
-        print('Tree end')
-    #global reward
-    reward = json_data['reward']
-    '''
-    print()
-    print('Children = ' + str(children))
-    print()
-    print('Reward = ' + str(reward))
-    '''
-    #global reward_list
-    reward_list.append(reward)
     
-    #return list(next_url), children, reward
 
-reward_list = []
-next_url = []
-
+    
+#reward_list = []
+urls = []
+reward_lst = []
 input_url = 'http://algo.work/interview/a'
-next_url.append(input_url)
+urls.append(input_url)
 
 start = time.time()
 
-for url in next_url:
-    fetch(url)
+if __name__ == '__main__':
+    processes = [mp.Process(target=fetch, args=(urls,reward_lst)) for x in range (4)]
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
+   
+#for url in next_url:
+ #   fetch(url)
  
 #t = threading.Thread(target = fetch, args = (next_url,))
  
@@ -54,12 +67,14 @@ for url in next_url:
 
 #print(next_url)
 #print(reward_list)
-final_ans = sum(reward_list)
+#final_ans = sum(reward_lst)
 
 end = time.time()
-
+print('Time = ' + str(end-start))
+'''
 print()
 print('****************************************')
 print('The toal sum of rewards is = ' + str(final_ans))
 print('****************************************')
 print('Time = ' + str(end-start))
+'''
