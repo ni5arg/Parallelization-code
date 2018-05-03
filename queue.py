fb278ac129be18a5739792a0cc1dc83d317585c7
ni5arg <35376387+ni5arg@users.noreply.github.com>
2018-05-03 11:46:36 -0300

    Using queue

---
+++ queue.py
@@ -0,0 +1,61 @@
+
+
+import threading
+import queue
+import requests
+import time
+
+def fetch_worker(url_q, reward_list):
+    while True:
+        try:
+            url = url_q.get()
+
+            # controller requests exit
+            if url is None:
+                return
+
+            # get json data
+            json_data = requests.get(url).json()
+
+            # queue more url_q tasks
+            for child in json_data.get('children', []): #No duplicate children
+                url_q.put(child)
+
+            # add reward to list
+            reward_list .append(json_data['reward'])
+        finally:
+            url_q.task_done()
+
+
+
+def fetch(url):
+    NUM_WORKERS = 96  # Not much improvement from 64, not too bad compared to 128.
+    reward_list = []
+    url_q = queue.Queue()
+    threads = [threading.Thread(target=fetch_worker, args=(url_q, reward_list))
+        for _ in range(NUM_WORKERS)]
+    for t in threads:
+        t.start()
+    url_q.put(url)
+    # wait for url and all subordinate urls to process
+    url_q.join()
+    # kill the workers
+    for _ in range(NUM_WORKERS):
+        url_q.put(None)
+    for t in threads:
+        t.join()
+    return sum(reward_list)
+
+
+input_url = input('Enter a URL. \n')#http://algo.work/interview/a
+start = time.time()
+
+ans = fetch(input_url)
+
+end = time.time()
+
+print('*************************')
+print('Input URL = ' + input_url)
+print('Sum of rewards =' + str(ans))
+print('Time taken = ' + str(end-start))
+print('*************************')
